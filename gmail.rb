b204dfcff8077df57fe5129dfd7e2bf158f23569
fetraharinjatovo <harinjatovo.fetra@gmail.com>
2019-06-19 11:50:33 +0300

    delete mail

---
+++ gmail.rb
@@ -0,0 +1,28 @@
+require 'gmail'
+require 'dotenv'
+
+def connect(username,password)
+    gmail = Gmail.connect(username,password)
+    if gmail.logged_in?
+    return gmail
+    else
+        return false
+    end
+end
+
+def delete_unready()
+    if connect() != :null
+        gmail = connect()
+        unread_email = gmail.inbox.find(:unread)
+        i = 0
+        puts "========== unread email : #{unread_email.count} ============="
+        unread_email.each do |email|
+            puts "==== delete email number #{i} #{email} ==="
+            email.delete!
+            i.next
+        end
+        puts "=== end delete"
+        
+    end
+
+end
